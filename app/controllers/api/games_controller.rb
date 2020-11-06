09882b04f1ee9134d32575e23e001a577e51adb3
shane <shanecolbyzero8@gmail.com>
2020-11-06 07:12:16 -0600

    added name with conditioal

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,5 +1,12 @@
 class Api::GamesController < ApplicationController
   def tell_name
+    @name = params[:name].upcase
+    p @name
+    if @name[0] == "A"
+      @message = "You have a name that starts with the first letter of the alphabet"
+    else
+      @message = ""
+    end
     render "name.json.jb"
   end
 end
